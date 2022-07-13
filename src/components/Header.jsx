6e674bbf3dd45de23a316b40c8cbdacb51bc3f02
yuridevweb <yuridevweb@gmail.com>
2022-07-13 16:08:06 +0100

    logout link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+// import React from "react";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import AuthContext from "../context/AuthContext";
 
 const Header = () => {
+  let { user, logoutUser } = useContext(AuthContext);
   return (
     <div>
       <Link to="/">Home</Link>
       <span> | </span>
-      <Link to="/login">Login</Link>
+      {user ? (
+        <p onClick={logoutUser}>Logout</p>
+      ) : (
+        <Link to="/login">Login</Link>
+      )}
+
+      {user && <p>{user.username} </p>}
     </div>
   );
 };
